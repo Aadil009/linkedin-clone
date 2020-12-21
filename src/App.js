f12076834dc18c9bb0090d0efbc178e34b331151
Aadil009 <sayyedadil8551@gmail.com>
2020-12-21 13:54:12 +0530

    added login and registration features

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import React from "react";
-import {useSelector} from 'react-redux'
+import React,{useEffect} from "react";
+import { useSelector , useDispatch } from "react-redux";
 import "./App.css";
-import { selectUser } from "./features/userSlice";
+import { selectUser,login,logout } from "./features/userSlice";
 import Feed from "./Feed";
+import { auth } from "./firebase";
 import Header from "./Header";
 import Login from "./Login";
 import Sidebar from "./Sidebar";
 
 function App() {
   const user = useSelector(selectUser);
-
+  const dispatch = useDispatch();
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        //logged in
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+            photoUrl: userAuth.photoURL,
+          })
+        );
+      } else {
+        //user logged out
+        dispatch(logout());
+      }
+    });
+  }, []);
   return (
     <div className="app">
       {/* Header */}
--- src/Login.js
+++ src/Login.js
@@ -1,15 +1,56 @@
 import React, { useState } from "react";
 import "./Login.css";
+import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
+import { login } from "./features/userSlice";
+
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
   const [profilePic, setProfilePic] = useState("");
+  const dispatch = useDispatch();
+
   const loginToApp = (e) => {
     e.preventDefault();
+
+    auth.signInWithEmailAndPassword(email,password)
+    .then(userAuth =>{
+      dispatch(login({
+        email:userAuth.user.email,
+        uid: userAuth.user.id,
+        displayName: userAuth.user.displayName,
+        profileUrl: userAuth.user.photoURL,
+      }))
+    }).catch(error =>alert(error));
   };
 
-  const register = () => {};
+  const register = () => {
+    if (!name) {
+      return alert("Please enter a full name");
+    }
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userAuth) => {
+        userAuth.user
+          .updateProfile({
+            displayName: name,
+            photURL: profilePic,
+          })
+          .then(() => {
+            dispatch(
+              login({
+                email: userAuth.user.email,
+                uid: userAuth.user.uid,
+                displayName: name,
+                photoUrl: profilePic,
+              })
+            );
+          });
+      })
+      .catch((error) => alert(error));
+  };
 
   return (
     <div className="login">
@@ -17,10 +58,10 @@ function Login() {
         src="https://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c528.png"
         alt=""
       />
-      <form className="">
+      <form>
         <input
           value={name}
-          onchange={(e) => setName(e.target.value)}
+          onChange={(e) => setName(e.target.value)}
           placeholder="Full name required"
           type="text"
         />
